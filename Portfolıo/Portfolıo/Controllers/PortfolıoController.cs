b4a70099302b376645acb59e6f322f914fa4ef1c
NuhDemir <drmaa217@gmail.com>
2024-06-26 21:27:43 +0300

    new coding

---
+++ Portfolıo/Portfolıo/Controllers/PortfolıoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolıo.Controllers
+{
+    public class PortfolıoController : Controller
+    {
+        // GET: Portfolıo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolıo/Portfolıo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Portfolıo.MvcApplication" Language="C#" %>
